163c799ea59eed682154cbb6c2810508260cc43d
Yadier <yadierg7@gmail.com>
2024-01-13 16:59:14 -0400

    improved ui

--- app/devlink/profile/page.tsx
+++ app/devlink/profile/page.tsx
@@ -3,14 +3,25 @@ import React from "react";
 
 export default function page() {
   return (
-    <div className="min-h-screen bg-[#f8f8f8] text-gray-700">
-      <section className="bg-white m-4 p-4">
+    <div className="min-h-screen flex bg-[#f8f8f8] text-gray-700">
+      <section className="bg-white rounded-lg h- grow-[2] m-3 hidden 2xl:flex">
+        s
+      </section>
+      <section className="bg-white m-4 p-4 grow-[1] ">
         <h1 className="font-bold text-black text-2xl">Profile Details</h1>
         <p>Add your details to create a personal touch to your profile. </p>
-        <div className=" bg-[#faf8f8] my-8 p-3 rounded-lg">
+        <div className=" bg-[#faf8f8] my-8 p-4 gap-4 flex flex-col  justify-center w-full xl:flex-row xl:justify-between items-center  rounded-lg">
           <h3>Profile Picture</h3>
-          <p>Image must be below 1024x1024px use PNG, JPG or BMP format.</p>
-          <img src="/pfp.jpg" alt="" />
+          <span className=" relative   ">
+            <img src="/pfp.jpg" alt="" className="rounded-lg h-52  xl:h-80 " />
+            <span className="absolute inset-0 text-white font-bold text-lg  rounded-lg bg-black transition-all opacity-0 hover:opacity-50 flex items-center justify-center">
+              Change Image
+            </span>
+          </span>
+          <div>
+            <p>Image must be below 1024x1024px </p>
+            <p>Use PNG, JPG or BMP format.</p>
+          </div>
         </div>
         <ProfileForm />
       </section>
