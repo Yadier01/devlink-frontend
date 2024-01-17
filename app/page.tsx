7dc3ba09dee220ba9a56ef5933062581f99b6527
Yadier <yadier@gmail.com>
2024-01-16 23:46:45 -0400

    removed router push to redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-"use client";
 
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
 export default function Home() {
-  const router = useRouter();
 
-  router.push("/login");
+  redirect("/login");
   return (
     <main className="flex min-h-screen bg-[#fafafa] flex-col items-center justify-center "></main>
   );
