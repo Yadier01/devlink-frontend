4b5a0b0cf4744adc4d58dab30bfd7fd6ee0a4571
Yadier <yadier@gmail.com>
2024-01-18 11:28:51 -0400

    made page server side render

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,31 +1,11 @@
-"use client";
 import React from "react";
 import { Form } from "../components/Form";
 import axios from "axios";
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 export default function page() {
-  const router = useRouter();
-  const constRegiser = async (name: string, password: string) => {
-    if (!name || !password) return;
-    try {
-      const response = await axios.post("https://devlink-backend-production.up.railway.app/auth/register", {
-        name,
-        password,
-      });
-
-      if (response.status === 201) {
-        <p>succes</p>;
-        setTimeout(() => {
-          router.push("/login");
-        }, 3000);
-      }
-    } catch (error: any) {
-      return error.response.data.error;
-    }
-  };
   return (
     <main className="min-h-screen flex items-center justify-center bg-[#fafafa]">
-      <Form onSubmitFetch={constRegiser} isLogin={false}></Form>
+      <Form  isLogin={false}></Form>
     </main>
   );
 }
