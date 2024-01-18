09c69251842871e6c4ee3b65c32dd10e5cfd9fb7
Yadier <yadier@gmail.com>
2024-01-18 11:37:57 -0400

    eliminated unlogin will add later

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -12,12 +12,6 @@ export const Login = ({}) => {
   const [data, setData] = React.useState<any>(null);
   const [error, setError] = React.useState<any>(null);
   const router = useRouter();
-const isToken = Cookies.get("token");
-  const deleteLocalStorage = () => {
-    Cookies.remove("token");
-    router.refresh();
-    router.push("/login");
-  };
 
   const onSubmitHanlderLogin = (e: any) => {
     e.preventDefault();
@@ -48,68 +42,64 @@ const isToken = Cookies.get("token");
   }, [data]);
   return (
     <>
-      {isToken ? (
-        <button onClick={deleteLocalStorage}>Log out</button>
-      ) : (
-        <form
-          className="flex bg-white p-8 gap-4 flex-col max-w-md h-[400px] justify-between  w-full rounded-lg  shadow-sm "
-          onSubmit={(e) => onSubmitHanlderLogin(e)}
-        >
-          <div>
-            <h2 className="text-2xl font-bold">Login</h2>
-            <p className="text-sm text-gray-500">
-              Add your details below to get back into the app
-            </p>
-          </div>
-          <span className="flex flex-col">
-            <label htmlFor="name" className="text-sm text-gray-700">
-              Name
-            </label>
-            <input
-              name="name"
-              type="text"
-              value={name}
-              placeholder="Enter your name"
-              className="border-2 border-gray-200 p-2 rounded-lg"
-              onChange={(e: any) => setName(e.target.value)}
-            />
-          </span>
-          <span className="flex flex-col">
-            <label
-              htmlFor="password"
-              className="text-sm
+      <form
+        className="flex bg-white p-8 gap-4 flex-col max-w-md h-[400px] justify-between  w-full rounded-lg  shadow-sm "
+        onSubmit={(e) => onSubmitHanlderLogin(e)}
+      >
+        <div>
+          <h2 className="text-2xl font-bold">Login</h2>
+          <p className="text-sm text-gray-500">
+            Add your details below to get back into the app
+          </p>
+        </div>
+        <span className="flex flex-col">
+          <label htmlFor="name" className="text-sm text-gray-700">
+            Name
+          </label>
+          <input
+            name="name"
+            type="text"
+            value={name}
+            placeholder="Enter your name"
+            className="border-2 border-gray-200 p-2 rounded-lg"
+            onChange={(e: any) => setName(e.target.value)}
+          />
+        </span>
+        <span className="flex flex-col">
+          <label
+            htmlFor="password"
+            className="text-sm
           text-gray-700
           "
-            >
-              Password
-            </label>
-            <input
-              placeholder="Enter your password"
-              name="password"
-              className="border-2 border-gray-200 p-2 rounded-lg"
-              value={password}
-              onChange={(e: any) => setPassword(e.target.value)}
-            />
-          </span>
-          <p className="text-center text-gray-500">
-            use user user if you don't want to register
-          </p>
-          <button
-            type="submit"
-            onClick={loginHandler}
-            className="bg-[#643cff] text-white p-2 rounded-lg font-bold"
           >
-            Login
-          </button>
-          <p className="text-gray-500 text-center ">
-            Don't have an account?
-            <Link href={"/register"} className="text-[#643cff] ml-2">
-              Create account
-            </Link>
-          </p>
-          {error && <p className="text-red-500 text-center ">{error}</p>}
-        </form>
-      )}
+            Password
+          </label>
+          <input
+            placeholder="Enter your password"
+            name="password"
+            className="border-2 border-gray-200 p-2 rounded-lg"
+            value={password}
+            onChange={(e: any) => setPassword(e.target.value)}
+          />
+        </span>
+        <p className="text-center text-gray-500">
+          use user user if you don't want to register
+        </p>
+        <button
+          type="submit"
+          onClick={loginHandler}
+          className="bg-[#643cff] text-white p-2 rounded-lg font-bold"
+        >
+          Login
+        </button>
+        <p className="text-gray-500 text-center ">
+          Don't have an account?
+          <Link href={"/register"} className="text-[#643cff] ml-2">
+            Create account
+          </Link>
+        </p>
+        {error && <p className="text-red-500 text-center ">{error}</p>}
+      </form>
     </>
   );
 };
