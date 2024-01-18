4f5eed9810cc4132ad5eaf8bf2b3a27c3654c354
Yadier <yadier@gmail.com>
2024-01-18 11:53:11 -0400

    fixed vercel error

--- app/components/Nolink.tsx
+++ app/components/Nolink.tsx
@@ -6,12 +6,12 @@ export default function Nolink() {
     <div className=" h-full flex items-center justify-center flex-col p-20  bg-[#fafafa]">
       <NoLinkSvg />
       <h2 className="text-2xl xl:text-3xl text-center font-bold my-6">
-        Let's get you started
+        Let&apos;s get you started
       </h2>
       <p className=" xl:w-3/5 text-gray-500  text-center">
         Use the “Add new link” button to get started. Once you have more than
-        one link, you can reorder and edit them. We're here to help you share
-        your profiles with everyone!
+        one link, you can reorder and edit them. We&apos;re here to help you
+        share your profiles with everyone!
       </p>
     </div>
   );
