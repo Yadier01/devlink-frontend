a4b0182b01077150768caac522104fa4cce256d4
Yadier <yadier@gmail.com>
2024-01-18 11:38:41 -0400

    improved ui and added functionality

--- app/components/Links.tsx
+++ app/components/Links.tsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import cookies from "js-cookie";
 import React, { useEffect, useState } from "react";
 import fetchUserInfo from "../hooks/fetchUserInfo";
+import Button from "./Button";
+import Nolink from "./Nolink";
 
 const URL = "https://devlink-backend-production.up.railway.app/";
 const plataformOption = ["Github", "Youtube", "Linkedin", "Twitter", "Gitlab"];
@@ -23,35 +25,22 @@ export const Links = () => {
     }
   };
 
-  const editUserInfo = async () => {
-    try {
-      const response = await axios.patch(URL, {
-        links,
-        token,
-      });
-      console.log(response.data);
-    } catch (error: any) {
-      console.log(error.response.data.error);
-    }
-  };
-
   const buttonHandler = () => {
+    if (links.length === 0) return;
     sendLinks();
-    console.log(links);
   };
 
   const newLinkComponentHandler = () => {
-    if (links.length >= 4) return;
     setLinks((prev) => [...prev, { url: "", platform: plataformOption[0] }]);
   };
 
-  const handleUrlChange = (idx, url) => {
+  const handleUrlChange = (idx: number, url: string) => {
     const newLinks = [...links];
     newLinks[idx].url = url;
     setLinks(newLinks);
   };
 
-  const handlePlatformChange = (idx, platform) => {
+  const handlePlatformChange = (idx: number, platform: string) => {
     const newLinks = [...links];
     newLinks[idx].platform = platform;
     setLinks(newLinks);
@@ -62,67 +51,78 @@ export const Links = () => {
     setLinks(newLinks);
   };
   return (
-    <div className="flex flex-col justify-between min-h-screen ">
-      <button
-        onClick={newLinkComponentHandler}
-        className="text-center w-full text-[#643cff] bg-white border-[#643cff] border  p-2 rounded-lg font-bold"
-      >
-        + Add new link
-      </button>
-      {links.length == 0 ? (
-        <p>No links :(</p>
-      ) : (
-        links.map((link, idx) => {
-          return (
-            <div
-              className="bg-[#f8f8f8]  p-4 my-4 rounded-lg text-gray-700"
-              key={idx}
-            >
-              <span className="flex justify-between items-center">
-                <h3 className="font-semibold">Link #{idx + 1}</h3>
-                <button onClick={() => removeLinkHandler(idx)}>Remove</button>
-              </span>
-              <span className="flex flex-col gap-1">
-                <span className="flex flex-col">
-                  <label htmlFor="plataform" className="text-xs">
-                    Platform
-                  </label>
-                  <select
-                    name="plataform"
-                    value={link.platform}
-                    className="bg-white border-2 border-gray-200 p-2 rounded-lg "
-                    onChange={(e) => handlePlatformChange(idx, e.target.value)}
-                  >
-                    {plataformOption.map((platform) => (
-                      <option key={platform}>{platform}</option>
-                    ))}
-                  </select>
-                </span>
-                <span className="flex flex-col ">
-                  <label htmlFor="Link " className="text-xs">
-                    Link
-                  </label>
+    <>
+      <section className="relative px-8 pt-8 ">
+        <div>
+          <h1 className="font-bold text-2xl ">Customize your links</h1>
+          <p className="text-gray-500 mt-2 mb-10">
+            Add/edit/remove links below and then share all your profilse with
+            the world!
+          </p>
+          <button
+            onClick={newLinkComponentHandler}
+            className="text-center w-full mb-4 text-[#643cff] bg-white border-[#643cff] border  p-2 rounded-lg font-bold"
+          >
+            + Add new link
+          </button>
+        </div>
+        <div className="overflow-y-auto h-[550px]  rounded-xl bg-white">
+          {links.length == 0 ? (
+            <Nolink />
+          ) : (
+            links.map((link, idx) => {
+              return (
+                <div
+                  className="bg-[#f8f8f8] w-full  p-4 my-4 rounded-lg text-gray-700"
+                  key={idx}
+                >
+                  <span className="flex justify-between items-center">
+                    <h3 className="font-semibold">Link #{idx + 1}</h3>
+                    <button onClick={() => removeLinkHandler(idx)}>
+                      Remove
+                    </button>
+                  </span>
 
-                  <input
-                    name="Link"
-                    className="border-2 border-gray-200 p-2 rounded-lg"
-                    type="text"
-                    value={link.url}
-                    onChange={(e) => handleUrlChange(idx, e.target.value)}
-                  />
-                </span>
-              </span>
-            </div>
-          );
-        })
-      )}
+                  <div className="flex flex-col gap-1">
+                    <span className="flex flex-col">
+                      <label htmlFor="plataform" className="text-xs">
+                        Platform
+                      </label>
+                      <select
+                        name="plataform"
+                        value={link.platform}
+                        className="bg-white border-2 border-gray-200 p-2 rounded-lg "
+                        onChange={(e) =>
+                          handlePlatformChange(idx, e.target.value)
+                        }
+                      >
+                        {plataformOption.map((platform) => (
+                          <option key={platform}>{platform}</option>
+                        ))}
+                      </select>
+                    </span>
 
-      <button
-        onClick={buttonHandler}
-        className="bg-[#633bfe] w-full text-white p-2 rounded-lg font-bold "
-      >
-        save
-      </button>
-    </div>
+                    <span className="flex flex-col ">
+                      <label htmlFor="Link " className="text-xs">
+                        Link
+                      </label>
+
+                      <input
+                        name="Link"
+                        className="border-2 border-gray-200 p-2 rounded-lg"
+                        type="text"
+                        value={link.url}
+                        onChange={(e) => handleUrlChange(idx, e.target.value)}
+                      />
+                    </span>
+                  </div>
+                </div>
+              );
+            })
+          )}
+        </div>
+      </section>
+      <Button onClick={buttonHandler} links={links} />
+    </>
   );
 };
