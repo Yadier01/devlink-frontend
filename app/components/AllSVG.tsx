f406e37c23bd55e60b2495a167354110d3f8cd36
yadier <yadierg7@gmail.com>
2025-05-20 16:57:52 -0700

    fixed error

--- app/components/AllSVG.tsx
+++ app/components/AllSVG.tsx
@@ -103,12 +103,10 @@ export function SVGArrrowRight() {
   );
 }
 
-import React from "react";
-
 export const SVGDropdownOpen = () => {
   return (
     <svg width="14" height="9" fill="none" viewBox="0 0 14 9">
-      <path stroke="#633CFF" stroke-width="2" d="m1 1 6 6 6-6"></path>
+      <path stroke="#633CFF" strokeWidth="2" d="m1 1 6 6 6-6"></path>
     </svg>
   );
 };
