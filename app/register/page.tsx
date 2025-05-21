474405a9ee757a259d80401d93b3b7e2e9b45e5e
yadier <yadierg7@gmail.com>
2025-05-20 17:11:11 -0700

    remove stuff

--- app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Form } from "../components/Form";
-export default function Page() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-[#fafafa]">
-      <Form isLogin={false}></Form>
-    </main>
-  );
-}
